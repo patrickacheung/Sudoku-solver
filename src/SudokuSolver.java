3578fe8bf7c161e16fcfb6e939f66af92fb1633e
Patrick Cheung <patrickcheungdev@gmail.com>
2015-12-20 17:05:17 -0800

    init commit

---
+++ src/SudokuSolver.java
@@ -0,0 +1,9 @@
+/**
+ * A simple command line sudoku solver using recursive backtracking
+ *
+ * @author Patrick Cheung
+ * @version 1.0
+ * Created by patch on 2015-12-20.
+ */
+public class SudokuSolver {
+}
