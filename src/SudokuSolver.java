33dfb8186c60cf111fc89ca0a9ef7c65a6edcce0
Patrick Cheung <patrickcheungdev@gmail.com>
2015-12-21 20:38:26 -0800

    added sudoku grid

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -5,5 +5,18 @@
  * @version 1.0
  * Created by patch on 2015-12-20.
  */
-public class SudokuSolver {
-}
+class SudokuSolver {
+    public static void main(String[] args){
+        // 9x9 sudoku grid
+        // 0 means empty cell
+        int grid[][] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                        {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                        {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                        {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                        {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                        {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                        {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+    }// end main
+}// end SudokuSolver
