6c993986be2c7b74c4fcf481efbba5b361480e9f
Patrick Cheung <patrickcheungdev@gmail.com>
2015-12-21 21:10:43 -0800

    added print function

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -6,8 +6,20 @@
  * Created by patch on 2015-12-20.
  */
 class SudokuSolver {
+    //sudoku grid size of NxN
+    private final int N = 9;
+
+    /* function for printing grid */
+    private static void printSolution(int grid[][]){
+        for(int i = 0; i < N; ++i){
+            for(int j = 0; j < N; ++j){
+                System.out.println(grid[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args){
-        // 9x9 sudoku grid
         // 0 means empty cell
         int grid[][] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
                         {5, 2, 0, 0, 0, 0, 0, 0, 0},
@@ -18,5 +30,9 @@ class SudokuSolver {
                         {1, 3, 0, 0, 0, 0, 2, 5, 0},
                         {0, 0, 0, 0, 0, 0, 0, 7, 4},
                         {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+
+        //need to solve it
+        //then if solved, print the grid
+        printSolution(grid);
     }// end main
 }// end SudokuSolver
